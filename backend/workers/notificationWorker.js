d475db5e7fdc8b3f9ebdc393aae59e9b030e74a7
Nutthakit <nutthakit2541@hotmail.com>
2025-06-17 18:36:14 +0700

    create-worker

--- backend/workers/notificationWorker.js
+++ backend/workers/notificationWorker.js
@@ -10,9 +10,31 @@ mongoose.connect(process.env.MONGODB_URL);
 
 const connection = new IORedis({
   host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_POST || 6379,
+  port: process.env.REDIS_PORT || 6379,
+  maxRetriesPerRequest: null,
 });
 
-const worker = new Worker("notificationQueue", async (job) => {
-  const { notificationId } = job.data;
+const worker = new Worker(
+  "notificationQueue",
+  async (job) => {
+    const { notificationId } = job.data;
+    const noti = await Notification.findById(notificationId).populate("user");
+
+    if (noti) {
+      console.log(
+        `Sending notification to user: ${noti.user.username} - ${noti.message}`
+      );
+    } else {
+      console.log(`Notification not found: ${notificationId}`);
+    }
+  },
+  { connection }
+);
+
+worker.on("completed", (job) => {
+  console.log(`Job ${job.id} complated`);
+});
+
+worker.on("failed", (job, err) => {
+  console.error(`Job ${job.id}`);
 });
